a4ae265f4c29d0b9daad2c333076e3d07fa3d9c7
Sukhjinder-Kaur <skaur831@my.centennialcollege.ca>
2020-07-13 13:22:51 -0400

    Modified Program.cs

--- Assignment 03/Program.cs
+++ Assignment 03/Program.cs
@@ -5,21 +5,24 @@ using System.Text;
 /*Name=Sukhjinder Kaur
 Student number= 301087895
 Date last Modified = 13 / 07 / 2020
-Program description=This is a “Driver” class used to create 
-an object instance of the GiantPlanet and TerrestrialPlanet classes*/
+Program description=This is a “Driver” class used to create an object instance of the GiantPlanet and TerrestrialPlanet classes*/
+
 namespace Assignment_03
 {
     public class Program
     {
         static void Main()
         {
-            Console.WriteLine("         Giant Planet description \n");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("         Giant Planet description ");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-            GiantPlanet giantPlanet = new GiantPlanet("Neptune", 49244,1.024, "Ice");
+            GiantPlanet giantPlanet = new GiantPlanet("Neptune", 49244, 1.024, "Ice");
             Console.WriteLine(giantPlanet.ToString());
 
-            Console.WriteLine("\n\n   Terrestrial Planet description \n ");
-
+            Console.WriteLine("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("       Terrestrial Planet description ");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Mars", 6779, 6.39, true);
             Console.WriteLine(terrestrialPlanet.ToString());
             WaitForAnyKey();
